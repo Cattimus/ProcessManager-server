b8d431932b036cb00c28f2fe17b53f07ca8c20bb
Cattimus <devcattimus@gmail.com>
2021-10-03 20:33:09 -0700

    ManagedProcess has been refactored
    ManagedProcess has been reworked to reduce the possibilities of invalid
    state and to handle it's own errors. the start() function now will
    display an error on failure instead of making the caller handle the
    error. Tracking has been added to prevent invalid calls if the process
    isn't running.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,20 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
 	public static void main(String[] args) {
+		ManagedProcess test = new ManagedProcess("proc/main");
+
+		test.io.write("Test");
+
 		try {
-			ManagedProcess test = new ManagedProcess("proc/main");
-
-			test.io.write("Test");
-
 			TimeUnit.MILLISECONDS.sleep(250);
-
-			if(test.io.readBufferSize() > 0) {
-				System.out.println(test.io.readLine());
-			}
-
-			test.stop();
-		} catch(IOException e) {
-			e.printStackTrace();
 		} catch(InterruptedException e) {
 			e.printStackTrace();
 		}
+
+		if(test.io.readBufferSize() > 0) {
+			System.out.println(test.io.readLine());
+		}
+		test.stop();
 
 		System.out.println("Program execution is finished.");
 	}
--- src/main/java/ManagedProcess.java
+++ src/main/java/ManagedProcess.java
@@ -6,17 +6,18 @@ import java.util.List;
 public class ManagedProcess {
 	public IOManager io = null;
 	private Process proc = null;
+	private boolean running = false;
 	private final List<String> processArgs;
 
 	//TODO - sanitize input before arbitrarily executing processes
-	ManagedProcess(String procName) throws IOException {
+	ManagedProcess(String procName) {
 		processArgs = new ArrayList<>();
 		processArgs.add(procName);
 
 		start();
 	}
 
-	ManagedProcess(String procName, String[] procArgs) throws IOException {
+	ManagedProcess(String procName, String[] procArgs) {
 		processArgs = new ArrayList<>();
 		processArgs.add(procName);
 		processArgs.addAll(Arrays.asList(procArgs));
@@ -25,33 +26,47 @@ public class ManagedProcess {
 	}
 
 	public void stop() {
-		if(io != null && proc != null) {
+		if(running) {
 			io.destroy();
 			proc.destroy();
 			proc = null;
 			io = null;
+			running = false;
+		} else {
+			System.err.println("Process: " + processArgs.get(0) + " is not running and will not be stopped.");
 		}
 	}
 
-	public void start() throws IOException {
-		if(proc == null && io == null) {
-			ProcessBuilder temp = new ProcessBuilder(processArgs);
-			proc = temp.start();
-			io = new IOManager(proc.getOutputStream(), proc.getInputStream());
+	public void start() {
+		if(!running) {
+			try {
+				ProcessBuilder temp = new ProcessBuilder(processArgs);
+				proc = temp.start();
+				io = new IOManager(proc.getOutputStream(), proc.getInputStream());
+				running = true;
+			} catch (IOException e) {
+				System.err.println("Unable to start process: " + processArgs.get(0) + ".");
+				e.printStackTrace();
+				running = false;
+			}
+		} else {
+			System.err.println("Process: " + processArgs.get(0) + " is already running and will not be started.");
 		}
 	}
 
 	public void restart() {
-		stop();
-		try {
-			start();
-		} catch(IOException e) {
-			System.err.println("restart: Unable to restart process: " + processArgs.get(0) + ".");
-			e.printStackTrace();
+		if(running) {
+			stop();
 		}
+
+		start();
 	}
 
 	public long getPID() {
 		return proc.pid();
+	}
+
+	public boolean isRunning() {
+		return running;
 	}
 }
