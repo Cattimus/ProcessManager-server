c1a1596ab9e3a548e6f3e74380add4b91349cc7f
Cattimus <devcattimus@gmail.com>
2021-10-24 15:59:31 -0700

    Client class has been added
    The client class will manage connections to active clients.
    Authentication will happen on a separate thread in the main program.
    Once a socket has been successfully authenticated, it will become a
    client.

---
+++ src/main/java/Client.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+//TODO Connection manager
+//TODO - Communicate with client over protocol
+//TODO - Possibly custom message class to be re-implemented clientside as a protocol
+//TODO - Immediately reject any IP which provides an invalid token
+//TODO - Obfuscation of connection protocol to discourage port scanners
+//TODO - TLS sockets (essential for security)
+
+public class Client {
+	private Socket remote;
+	OutputStream out = null;
+	BufferedReader in = null;
+
+	//connection should already be accepted and authenticated from main by this point
+	Client(Socket remote) {
+		this.remote = remote;
+
+		try {
+			out = remote.getOutputStream();
+			in = new BufferedReader( new InputStreamReader(remote.getInputStream()));
+		} catch(IOException e) {
+			System.err.println("[MASTER]: client failed to connect properly.");
+		}
+
+		new Thread(this::listenThread).start();
+	}
+
+	//read input from socket
+	public void listenThread() {
+		try {
+			String msg = null;
+			while ((msg = in.readLine()) != null) {
+				System.out.println("Input recieved from client: " + msg);
+			}
+		} catch (IOException e) {
+			//force close on failure to read?
+		}
+	}
+
+	//write output to socket
+	public synchronized void write(String toSend) {
+		try {
+			out.write(toSend.getBytes(StandardCharsets.US_ASCII));
+			out.flush();
+		} catch(IOException e) {
+			//force close on failure to write?
+		}
+	}
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,21 @@
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-
-	//project-wide requirements
-	//TODO Connection manager
-	//TODO - Accept connection
-	//TODO - Communicate with client over protocol
-	//TODO - Possibly custom message class to be re-implemented clientside as a protocol
-	//TODO - Immediately reject any IP which provides an invalid token
-	//TODO - Obfuscation of connection protocol to discourage port scanners
-	//TODO - TLS sockets (essential for security)
 
 	public static class State {
 		public static String credentialsFile; //file where password hashes are stored
 		public static String logDirectory;    //directory where logs are written (if enabled)
 	}
 
-	public static void main(String[] args) throws InterruptedException{
-		Proc test = new Proc("test", "python3", "proc/main.py");
-		test.start();
+	public static void main(String[] args) throws IOException {
+		System.out.println("Listening for clients on socket 31243...");
+		ServerSocket server = new ServerSocket(31243);
+		var temp = server.accept();
+		System.out.println("New connection has been accepted on: " + temp.getLocalSocketAddress());
 
-		while(!test.isRunning()) {
-			TimeUnit.MILLISECONDS.sleep(1);
-		}
-
-		test.sendSignal("quit gracefully");
-
-		TimeUnit.MILLISECONDS.sleep(500);
-
-		test.stop();
-
+		Client test = new Client(temp);
+		test.write("Yep it worked.\n");
 	}
 }
